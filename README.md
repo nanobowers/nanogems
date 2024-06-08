ac356c9177674b6976407195f73cda1e1638cdd1
Ben Bowers <nanobowers@gmail.com>
2024-06-07 21:45:18 -0400

    initial checkin based on microgems and minigems

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # nanogems
-A minimalist rubygem loader
+A minimalist rubygem loader.  Useful for when ultra-fast startup time is important.
+
+## About
+
+Nanogems is intended to be a higher performance rubygem loader.  It does not provide the rubygems API, it just handles file loading. "No magic. No bloated code. Just Ruby."
+
+## Usage
+
+```sh
+ruby --disable-gems -rnanogems some.rb
+```
+
+Then use normal requires:
+
+```ruby
+require "optimist"
+```
+
+You may wish to use the `Kernel#gem` method to specify a version.  If used liberally it may also speed your load time.
+
+```ruby
+require "nanogems"
+gem "optimist", "3.0.0"
+require "optimist"
+```
+
+If you do not want to put `require "nanogems"` in all your scripts using stuff from gems, just place `export RUBYOPT=rnanogems` into your profile file (`/etc/profile` or `~/.profile`).
+
+## Operation and Tricks
+
+Using rubygems can be extremely slow for lightweight ruby scripts, especially in the following scenarios:
+
++ Communal Ruby installation over a shared network with lots of installed gems
++ Slow filer performance, e.g. cloud storage
++ Only a few gems are used and they fit "standard" patterns.
+
+nanogems exploits the fact that:
+1. Ultimately we just want to update `$LOAD_PATH` to find our gem code.
+2. Most standard rubygems install into: `#{gemname}-#{version}/lib`
+
+It also saves time by not reading gem specification files or doing a lot of other things rubygems does.
+
+## Installation
+
+:construction:
+
+This will install nanogems
+```sh
+sudo ruby install.rb
+```
+## Performance
+
+For small numbers of gems loaded or simple gems, performance should be better than rubygems.  If it is not, feel free to use rubygems or submit a fix via a pull-request.
+
+```sh
+### with rubygems (default)
+$ hyperfine "ruby -e \"require 'optimist'; puts Optimist::VERSION\""
+Benchmark 1: ruby -e "require 'optimist'; puts Optimist::VERSION"
+  Time (mean ± σ):      77.7 ms ±   2.3 ms    [User: 66.1 ms, System: 10.7 ms]
+  Range (min … max):    74.6 ms …  87.2 ms    37 runs
+
+### with nanogems
+$ hyperfine "ruby --disable-gems -r./nanogems -e \"require 'optimist'; puts Optimist::VERSION\""
+Benchmark 1: ruby --disable-gems -r./nanogems -e "require 'optimist'; puts Optimist::VERSION"
+  Time (mean ± σ):      20.0 ms ±   1.8 ms    [User: 14.7 ms, System: 5.0 ms]
+  Range (min … max):    18.0 ms …  26.0 ms    139 runs
+
+### with nanogems and gem version specification
+$ hyperfine "ruby --disable-gems -r./nanogems -e \"gem 'optimist', '3.1.0'; require 'optimist'; puts Optimist::VERSION\""
+Benchmark 1: ruby --disable-gems -r./nanogems -e "gem 'optimist', '3.1.0'; require 'optimist'; puts Optimist::VERSION"
+  Time (mean ± σ):      17.7 ms ±   1.7 ms    [User: 12.9 ms, System: 4.5 ms]
+  Range (min … max):    15.8 ms …  22.6 ms    170 runs
+``` 
+ 
+... your mileage may vary.
+
+## Bugs/Features
+
+Absolute gem version specifiers should work, but relative ones ">=0.a" are not supported.
+
+This may not cleanly load all gems, as it takes a lot of liberties for performance reasons.  Many modern gems rely on rubygems being loaded and all of the rubygems `Gem::*` API present.  These are not in scope for nanogems.
+
+If the required files aren't in `#{gemname}-#{version}/lib`, you may be able to patch around it by explicitly using `gem "<other_name>"`
+
+# Attribution
+
+This work is based on the fine work of some other projects:
++ [microgems](https://github.com/botanicus/microgems) 
++ [minigems](https://github.com/fabien/minigems)
+
+Authors for other projects are attributed in the [LICENSE](LICENSE)
---
+++ nanogems.rb
@@ -0,0 +1,184 @@
+
+unless $LOADED_FEATURES.find { |file| file.match(/rubygems/) }
+  require 'rbconfig'
+  module Gem
+    VERSION = "3.1.4" # arbitrary, but we need something.
+    WIN_PATTERNS = [/bccwin/i, /cygwin/i, /djgpp/i, /mingw/i, /mswin/i, /wince/i]
+    @@win_platform = nil
+    def self.win_platform?
+      if @@win_platform.nil? then
+        @@win_platform = !!WIN_PATTERNS.find { |r| RUBY_PLATFORM =~ r }
+      end
+      @@win_platform
+    end
+
+    # Gem::LoadError: Could not find RubyGem sdfsdf (>= 0)
+    class LoadError < ::LoadError
+    end
+  end
+
+  # load for rubygems version comparators
+  require 'rubygems/version'
+
+  class Nanogems
+    VERSION = "1.0.0"
+    class << self
+      
+      # TODO: configuration
+      # TODO: modularize, ~/.gems
+
+      # Nanogems.gempaths("foo")
+      # => ["/usr/lib/ruby/gems/1.8/gems/foo-1.0.0/lib"]
+      def gempaths(name, dirlist)
+        dirlist = self.path.flat_map { |gpath| self.dircache(File.join(gpath, 'gems', '*')) }
+        dirlist.select { |dir| File.basename(dir).match?(/^#{name}-([\d\.]+)$/)}.map { |d| File.join(d, 'lib')}
+        #self.path.flat_map do |gpath|
+        #  Dir["#{gpath}/gems/#{name}-*/lib"]
+        #end
+      end
+
+      def get_named_gem_path(name, version)
+        self.path.each do |gpath|
+          dirpath = "#{gpath}/gems/#{name}-#{version}/lib"
+          return dirpath if Dir.exist? dirpath
+        end
+        nil
+      end
+
+      def dircache(directory)
+        @dircache ||= {}
+        @dircache[directory] ||= Dir[directory]
+      end
+
+      def highest_gem(name)
+        lookup = {}
+        gem_paths = self.gempaths(name, nil)
+        raise LoadError.new("Cannot find gem #{name}") if gem_paths.empty? 
+        #p gem_paths_possible: gem_paths
+        gem_paths.each do |gem_path|
+          #p gempath: gem_path
+          if matches = File.basename(File.dirname(gem_path)).match(/^(.*?)-([\d\.]+)$/)
+            #p matches: matches
+            name, version_no = matches.captures[0,2]
+            version = Gem::Version.new(version_no)
+            if !lookup[name] || (lookup[name] && lookup[name][1] < version)
+              lookup[name] = [gem_path, version]
+            end
+          end
+        end
+        p name: name, lookup: lookup
+        return lookup[name][0]
+      end
+
+      # Nanogems.gem("foo", "0.9.8")
+      # => "/usr/lib/ruby/gems/1.8/gems/foo-0.9.8/lib"
+      def gem(name, version)
+        if version.nil?
+          self.highest_gem(name)
+        else
+          get_named_gem_path(name, version) || self.gem(name, nil)
+        end
+      end
+
+      def loaded_gems
+        @loaded_gems ||= Array.new
+      end
+
+      # Array of paths to search for Gems.
+      def path
+        @path ||= begin
+          paths = [self.default_path]
+          paths += ENV.fetch("GEM_PATH").split(File::PATH_SEPARATOR)
+          paths
+        end
+      end
+
+      def default_path
+        @default_path ||= if defined? RUBY_FRAMEWORK_VERSION then
+          File.join File.dirname(RbConfig::CONFIG["sitedir"]), 'Gems', 
+            RbConfig::CONFIG["ruby_version"]
+        elsif defined?(RUBY_ENGINE) && File.directory?(
+          File.join(RbConfig::CONFIG["libdir"], RUBY_ENGINE, 'gems', 
+            RbConfig::CONFIG["ruby_version"])
+          )
+            File.join RbConfig::CONFIG["libdir"], RUBY_ENGINE, 'gems', 
+              RbConfig::CONFIG["ruby_version"]
+        else
+          File.join RbConfig::CONFIG["libdir"], 'ruby', 'gems', 
+            RbConfig::CONFIG["ruby_version"]
+        end
+      end
+
+      def activate(name, version = nil)
+        gem = if version.nil?
+          self.highest_gem(name)
+        else
+          # TODO: > = ~ version
+          # >= 0
+          version.delete!("<>=~ ") if version.is_a?(String)
+          self.gem(name, version)
+        end
+        #p gem: gem
+        self.loaded_gems.push(File.basename(File.dirname(gem)))
+        $:.push(gem)
+      end
+
+      # We may not actually need this...
+      def deactivate(name, version = nil)
+        if version.nil?
+          $:.each do |path|
+            if File.dirname(path).match(/^#{Regexp::quote(name)}-/)
+              $:.delete!(path)
+            end
+          end
+        else
+          # TODO: > = ~ version
+          # >= 0
+          version.delete!("<>=~ ") if version.is_a?(String)
+          libdir = self.gem(name, version)
+          $:.delete(libdir) if $:.include?(libdir)
+        end
+      end
+
+      def path_info
+        if $DEBUG or $VERBOSE
+          STDERR.puts "Load paths: #{$:.inspect}"
+          STDERR.puts "Loaded gems: #{::Nanogems.loaded_gems.inspect}"
+        end
+      end
+    end
+  end
+
+  module Kernel
+    alias_method :__require__, :require
+
+    def gem(name, version = nil)
+      Nanogems.activate(name, version)
+    end
+    
+    # Attempt sequence for a require:
+    # 1. kernel.require
+    # 2. activate gemname-replace-slash-with-dash, then require
+    #   - rack/protection -> rack-protection
+    # 3. activate gemname-strip-after-slash, then require
+    #   - rexml/document -> rexml
+    def require(file)
+      __require__(file)
+    rescue LoadError
+      begin
+        hackfile = file.gsub(%r|/|,'-')
+        Nanogems.activate(hackfile) rescue nil
+        __require__(file)
+      rescue LoadError
+        begin
+          hackfile = file.sub(%r|/.*|,'')
+          Nanogems.activate(hackfile) rescue nil
+          __require__(file)
+        rescue LoadError
+          Nanogems.path_info
+          raise $!
+        end
+      end
+    end
+  end
+end
